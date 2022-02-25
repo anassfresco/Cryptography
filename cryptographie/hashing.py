368082a1fd6459ed00f65f17d9217a684c121d40
anaskirito <68207197+anassfresco@users.noreply.github.com>
2022-02-25 17:46:56 +0100

    Update hashing.py

--- cryptographie/hashing.py
+++ cryptographie/hashing.py
@@ -1,3 +1,10 @@
 from hashlib import md5
 from hashlib import sha256
-
+from hashlib import sha512
+s="anas is the best boy in the world"
+result_md5=md5(s.encode())
+result_sha256=sha256(s.encode())
+result_sha512=sha512(s.encode())
+print(result_md5.hexdigest())
+print(result_sha256.hexdigest())
+print(result_sha512.hexdigest())
